4f3700744c1007aefbab8159346054e6f415a03d
Yashiro <1161569428@qq.com>
2022-08-01 10:47:04 +0800

    Add dd call

--- src/consts.ts
+++ src/consts.ts
@@ -4,4 +4,5 @@
 require('dotenv').config();
 
 export const chainAddr = process.env.CHAIN_ADDR as string;
+export const ddurl = process.env.DDURL as string;
 export const offlineThreshold = Number(process.env.OFFLINETHRESHOLD as string);
--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,44 @@ import { mainnetApi } from "./api/crustMainnetApi"
 import { parseObj, sleep } from "./utils";
 import { logger } from '@polkadot/util';
 import _ from 'lodash';
-import { offlineThreshold } from "./consts";
+import { ddurl, offlineThreshold } from "./consts";
 import { ApiPromise } from "@polkadot/api";
 
 const l = logger('main');
 
 let currentEra = 1414;
 
+const axios = require('axios');
+
 const maybeDoDingdingcall = async (_api: ApiPromise, activeEra: number) => {
     const validatorCount = parseObj(await _api.query.staking.validatorCount());
 
     const erasRewardPoints = parseObj(await _api.query.staking.erasRewardPoints(activeEra));
     console.log('erasRewardPoints: ', erasRewardPoints.individual)
-    const result = Object.entries(erasRewardPoints.individual).map(e => e[1])
-    const offlineRate = _.divide(validatorCount - result.length, validatorCount);
+    const result = Object.entries(erasRewardPoints.individual).map(e => e[1]);
+    const offlineCount = validatorCount - result.length;
+    const offlineRate = _.divide(offlineCount, validatorCount);
     console.log('result: ', result.length)
     // const haha = _.countBy(result, e => e == 140)
     console.log('haha: ', offlineRate)
     console.log('haha > offlineThreshold: ', offlineRate > offlineThreshold);
     if (offlineRate > offlineThreshold) {
         // 掉线率大于掉线阈值时发送钉钉警报
+        const data = JSON.stringify({ "msgtype": "text", "text": { "content": `offline count: ${offlineCount}, validator count: ${validatorCount}, validator offlineRate: ${offlineRate}` }, "at": { "atMobiles": "['validator]", "isAtAll": false } });
+        const config = {
+            method: 'post',
+            url: ddurl,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: data
+        };
+
+        axios(config).then(function (response: { data: any; }) {
+            console.log(JSON.stringify(response.data));
+        }).catch(function (error: any) {
+            console.log(error);
+        });
     }
 }
 
