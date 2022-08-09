604e46420ccbf1049075c26b7c93f16884b30543
Yashiro <1161569428@qq.com>
2022-08-09 13:53:21 +0800

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { mainnetApi } from "./api/crustMainnetApi"
-import { parseObj, sleep } from "./utils";
+import { parseObj, sleep, unitToNumber } from "./utils";
 import { logger } from '@polkadot/util';
 import _ from 'lodash';
 import { ddurl, offlineThreshold } from "./consts";
 import { ApiPromise } from "@polkadot/api";
+import BN from 'bn.js';
 
 const l = logger('main');
 
@@ -13,19 +14,23 @@ const axios = require('axios');
 
 const maybeDoDingdingcall = async (_api: ApiPromise, activeEra: number) => {
     const validatorCount = parseObj(await _api.query.staking.validatorCount());
-
     const erasRewardPoints = parseObj(await _api.query.staking.erasRewardPoints(activeEra));
-    console.log('erasRewardPoints: ', erasRewardPoints.individual)
+    const stakeLimits = parseObj(await _api.query.staking.stakeLimit.entries());
+    const limits = [];
+    for (const [_, limit ] of stakeLimits) {
+        limits.push(unitToNumber(new BN(Number(limit).toString())))
+    }
+    const waitingCount = limits.filter(e => e > 0).length - validatorCount;
+    console.log('waitingCount: ', waitingCount)
     const result = Object.entries(erasRewardPoints.individual).map(e => e[1]);
     const offlineCount = validatorCount - result.length;
     const offlineRate = _.divide(offlineCount, validatorCount);
     console.log('result: ', result.length)
-    // const haha = _.countBy(result, e => e == 140)
-    console.log('haha: ', offlineRate)
-    console.log('haha > offlineThreshold: ', offlineRate > offlineThreshold);
+    console.log('offlineRate: ', offlineRate)
+    console.log('offlineRate > offlineThreshold: ', offlineRate > offlineThreshold);
     if (offlineRate > offlineThreshold) {
         // 掉线率大于掉线阈值时发送钉钉警报
-        const data = JSON.stringify({ "msgtype": "text", "text": { "content": `offline count: ${offlineCount}, validator count: ${validatorCount}, validator offlineRate: ${offlineRate}` }, "at": { "atMobiles": "['validator]", "isAtAll": false } });
+        const data = JSON.stringify({ "msgtype": "text", "text": { "content": `offline count: ${offlineCount}, validator count: ${validatorCount}, validator offlineRate: ${offlineRate}, waiting count: ${waitingCount}` }, "at": { "atMobiles": "['validator]", "isAtAll": false } });
         const config = {
             method: 'post',
             url: ddurl,
@@ -46,7 +51,7 @@ const maybeDoDingdingcall = async (_api: ApiPromise, activeEra: number) => {
         const minitus = nowTime.getMinutes();
         console.log("hour", hour)
         if(hour < 12 && hour >= 11 && minitus >= 0 && minitus <= 30) { 
-            const data = JSON.stringify({ "msgtype": "text", "text": { "content": `一切正常, offline count: ${offlineCount}, validator count: ${validatorCount}, validator offlineRate: ${offlineRate}` }, "at": { "atMobiles": "['validator]", "isAtAll": false } });
+            const data = JSON.stringify({ "msgtype": "text", "text": { "content": `一切正常, offline count: ${offlineCount}, validator count: ${validatorCount}, validator offlineRate: ${offlineRate}, waiting count: ${waitingCount}` }, "at": { "atMobiles": "['validator]", "isAtAll": false } });
             const config = {
                 method: 'post',
                 url: ddurl,
@@ -62,7 +67,6 @@ const maybeDoDingdingcall = async (_api: ApiPromise, activeEra: number) => {
             });
         }
     }
-
 }
 
 const getNodeStatus = async () => {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,6 @@
+import BN from 'bn.js';
+
+const CRU_UNIT = new BN('1000000000');
 export const parseObj = (obj: any) => {
     return JSON.parse(JSON.stringify(obj));
 };
@@ -8,4 +11,9 @@ export function sleep(time: number) {
             resolve();
         }, time);
     });
+}
+
+export const unitToNumber = (unit: BN) => {
+    if (unit.isZero()) return 0;
+    return unit.div(CRU_UNIT).toNumber() / 1000.0;
 }
